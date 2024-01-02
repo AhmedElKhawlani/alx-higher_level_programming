ebbb9119606a46d2b35afdb8146de4103547e058
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-01-02 22:20:55 +0100

    Tasks 6, 7 & 8

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for d1 in range(8):
+    for d2 in range(d1 + 1, 10):
+        print('{}{},'.format(d1, d2), end=' ')
+print(89)
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    dif = ord('z') - ord('Z')
+    for c in str:
+        if 'a' <= c <= 'z':
+            o = ord(c) - dif
+        else:
+            o = ord(c)
+        print('{:c}'.format(o), end='')
+    print()
+    
