653d7e6aff72b1a0c7d66f94997f4b20d81d17be
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-01-10 17:29:10 +0100

    Correcting 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if not (isinstance(roman_string, str)) or roman_string is None:
         return 0
-    D = {'I': 1,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    D = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     s = 0
-    for x in roman_string:
-        s = s + D[x]
+    last = 0
+    i = len(roman_string) - 1
+    for j in range(i, -1, -1):
+        x = D[roman_string[j]]
+        if x >= last:
+            s = s + x
+        else:
+            s = s - x
+        last = x
     return s
