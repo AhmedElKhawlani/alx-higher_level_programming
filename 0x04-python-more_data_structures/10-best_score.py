b65b92162dc26efa4919fbbb715ad68bfdcc4b2b
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-01-10 17:09:27 +0100

    Correcting 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         key = None
         value = None
         for x in a_dictionary:
-            if key == None:
+            if key is None:
                 key = x
                 value = a_dictionary[x]
             elif a_dictionary[x] > value:
