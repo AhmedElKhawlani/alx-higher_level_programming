e1f5917fbbfda10f970e7be7a99f12241b803781
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-01-22 20:53:24 +0100

    Commiting 0, 1 & 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-	i = 0
-	cpt = 0
-	while i < x:
-		try:
-			print("{:d}".format(my_list[i]), end='')
-			cpt = cpt + 1
-		except TypeError:
-			pass
-		except ValueError:
-			pass
-		i = i + 1
-	if cpt > 0:
-		print()
-	return cpt
+    i = 0
+    cpt = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            cpt = cpt + 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        i = i + 1
+    if cpt > 0:
+        print()
+    return cpt
