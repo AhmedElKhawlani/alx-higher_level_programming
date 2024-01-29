d600fdab3c7ef2616d559332446b87b0a4ac8610
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-01-29 20:24:29 +0100

    Adding 0, 1, 2 & 3

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# 0-rectangle.py by EL KHAWLANI Ahmed
-""" Defines an empty class named Rectangle"""
+"""A class that defines rectangles"""
 
 
 class Rectangle:
-    """ Empty class that represent a Rectangle """
+    """An empty class that represents a rectangle"""
     pass
