52622f256d047a846ca2e886aeb17ab59d14167f
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-01-30 17:37:21 +0100

    Correcting 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.nbr_print = getattr(magic_string, 'nbr_print', 0)
+    magic_string.nbr_print = getattr(magic_string, 'nbr_print', 0) + 1
     return ', '.join(['BestSchool' for k in range(magic_string.nbr_print)])
