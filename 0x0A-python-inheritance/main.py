51a6064590d7f994994d000b8c4b21e2f00904a2
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-02-06 22:32:36 +0100

    Correcting 7

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-MyInt = __import__('100-my_int').MyInt
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-my_i = MyInt(3)
-print(my_i)
-print(my_i == 3)
-print(my_i != 3)
+bg = BaseGeometry()
+
+bg.integer_validator("my_int", (5,))
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
