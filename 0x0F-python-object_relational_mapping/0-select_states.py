e243ef8aa9223912183cd8d4546613fc48c61a26
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-06-25 21:54:49 +0100

    Tasks 0, 1 & 2)

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This script lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb as sql
+import os
+
+if __name__ == '__main__':
+    arg = os.argv
+    DB = sql.connect(host='localhost', user=arg[1], passwd=arg[2],
+                     db=arg[3], port=3306)
+    C = DB.cursor()
+    C.execute("SELECT * FROM states ORDER BY id")
+    records = C.fetchall()
+    for record in records:
+        print(record)
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This script lists all states with a name starting with N,
+from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb as sql
+import os
+
+if __name__ == '__main__':
+    arg = os.argv
+    DB = sql.connect(host='localhost', user=arg[1], passwd=arg[2],
+                     db=arg[3], port=3306)
+    C = DB.cursor()
+    C.execute("SELECT * FROM states ORDER BY id WHERE name = '" + arg[4] +"'")
+    records = C.fetchall()
+    for record in records:
+        print(record)
