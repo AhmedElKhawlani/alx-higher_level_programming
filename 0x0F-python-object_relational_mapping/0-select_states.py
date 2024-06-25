dc190167777646925be12e1df86ef64f95fb9988
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-06-25 22:08:09 +0100

    Debug 2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,15 @@
 This script lists all states from the database hbtn_0e_0_usa.
 """
 
-import MySQLdb as sql
+import MySQLdb
 import os
 
 if __name__ == '__main__':
     arg = os.argv
-    DB = sql.connect(host='localhost', user=arg[1], passwd=arg[2],
+    DB = MySQLdb.connect(host='localhost', user=arg[1], passwd=arg[2],
                      db=arg[3], port=3306)
     C = DB.cursor()
-    C.execute("SELECT * FROM states ORDER BY id")
+    C.execute("SELECT * FROM states")
     records = C.fetchall()
     for record in records:
         print(record)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,12 +5,12 @@ This script lists all states with a name starting with N,
 from the database hbtn_0e_0_usa.
 """
 
-import MySQLdb as sql
+import MySQLdb
 import os
 
 if __name__ == '__main__':
     arg = os.argv
-    DB = sql.connect(host='localhost', user=arg[1], passwd=arg[2],
+    DB = MySQLdb.connect(host='localhost', user=arg[1], passwd=arg[2],
                      db=arg[3], port=3306)
     C = DB.cursor()
     C.execute("SELECT * FROM states ORDER BY id WHERE name = '" + arg[4] + "'")
