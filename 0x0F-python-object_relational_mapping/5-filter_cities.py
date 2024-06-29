8f42e4582be81ab0469d960d643de7d34fdfd0b4
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-06-29 12:03:54 +0100

    Debug Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,13 +12,17 @@ if __name__ == '__main__':
     hs = 'localhost'
     DB = MySQLdb.connect(host=hs, user=a[1], passwd=a[2], db=a[3], port=3306)
     C = DB.cursor()
-    query1 = "SELECT cities.id, cities.name, states.name "
+    query1 = "SELECT cities.name "
     query2 = "FROM cities, states "
     query3 = "WHERE cities.state_id = states.id AND states.name = %s "
     query4 = "ORDER BY cities.id"
     C.execute(query1 + query2 + query3 + query4, (a[4],))
     records = C.fetchall()
-    for record in records:
-        print(record)
+    cities = [records[i][0] for i in range(len(records))]
+    for i in range(len(cities)):
+        if i == len(cities) - 1:
+            print(cities[i])
+        else:
+            print(cities[i], end=', ')
     C.close()
     DB.close()
