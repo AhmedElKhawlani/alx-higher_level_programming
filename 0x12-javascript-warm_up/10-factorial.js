a542cbd980480f9b151063b62ae6297256ff7331
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-07-26 13:42:55 +0100

    Fix 10 & 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 function fact (n) {
-    if (!n) {
-        return 1;
-    }
-    return n * fact(n - 1);
+  if (!n) {
+    return 1;
   }
+  return n * fact(n - 1);
+}
 
-  console.log(fact(process.argv[2]));
-  
+console.log(fact(process.argv[2]));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,25 +4,24 @@ const args = process.argv.slice(2);
 
 if (args.length === 0) {
   console.log(0);
-} else if (args.length === 1){
+} else if (args.length === 1) {
   console.log(0);
 } else {
-    let max1 = parseInt(args[0], 10);
-    let max2 = parseInt(args[1], 10);
-    if (max2 > max1){
-        let alt = max1;
-        max1 = max2;
-        max2 = alt;
-    }
-  for (let i = 2; i < args.length; i++){
-    n = parseInt(args[i], 10)
-    if (n > max1){
-        max2 = max1;
-        max1 = n;
-    }
-    else if (n > max2){
-        max2 = n;
-    }
-    }
-    console.log(max2);
+  let max1 = parseInt(args[0], 10);
+  let max2 = parseInt(args[1], 10);
+  if (max2 > max1) {
+    const alt = max1;
+    max1 = max2;
+    max2 = alt;
   }
+  for (let i = 2; i < args.length; i++) {
+    const n = parseInt(args[i], 10);
+    if (n > max1) {
+      max2 = max1;
+      max1 = n;
+    } else if (n > max2) {
+      max2 = n;
+    }
+  }
+  console.log(max2);
+}
