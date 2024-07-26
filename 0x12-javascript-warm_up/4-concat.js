68825b4f35b7d051535a2ea8d9d10930d4818215
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-07-26 11:55:46 +0100

    Adding semicolons to 0, 1, 2, 3 & 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,18 +3,18 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-    console.log("undefined is undefined")
+    console.log("undefined is undefined");
 }
 else {
     if (args.length === 1) {
-        let arg = args[0]
-        let show = arg + " is undefined"
-        console.log(show)
+        let arg = args[0];
+        let show = arg + " is undefined";
+        console.log(show);
     }
     else {
-        let arg1 = args[0]
-        let arg2 = args[1]
-        let show = arg1 + " is " + arg2
-        console.log(show)
+        let arg1 = args[0];
+        let arg2 = args[1];
+        let show = arg1 + " is " + arg2;
+        console.log(show);
     }
 }
