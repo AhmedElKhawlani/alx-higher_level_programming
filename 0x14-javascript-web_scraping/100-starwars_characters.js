4ebbcbc1668d480789f0e4be4ee089bac411e249
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-07-31 21:35:51 +0100

    Optional 1

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,13 +4,13 @@ const request = require('request');
 const API = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request.get(API, function (error, response, body) {
-  if (err) {
+  if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
     characters.forEach(character => {
       request.get(character, function (error, response, body) {
-        if (err) {
+        if (error) {
           console.log(error);
         } else if (response.statusCode === 200) {
           console.log(JSON.parse(body).name);
